78f26f1032ab63169607c9db440fe8e58f5d9a2f
Ivan Kr <126171248+Dofff95@users.noreply.github.com>
2023-11-13 20:05:08 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
-import user  from 'user.json';
-import Profile  from "/profile/Profile.js";
+// import user  from 'user.json';
+// import Profile  from "./profile/Profile";
 
-import Statistics from '/Statistics/Statistics';
+import Statistics from './Statistics/Statistics';
 import data  from 'data.json';
 
-import FriendList from '/FriendList/FriendList.js'
+import FriendList from './FriendList/FriendList'
 import friend from "friends.json";
 
 import transactions from "transactions.json";
-import TransactionHistory from "/TransactionHistory/TransactionHistory.js";
+import TransactionHistory from "./TransactionHistory/TransactionHistory.js";
 
 
  export const App = () => {

2a5ae5286fe5146458d2061bba286ea1477fb02b
Ivan Kr <126171248+Dofff95@users.noreply.github.com>
2023-11-14 22:23:00 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import user  from '../data/user.json';
-import  Profile  from './Profile/Profile';
+import  Profile  from './components/Profile/Profile';
 
 
 import Statistics from './Statistics/Statistics';
